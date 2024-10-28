d492b006c7cffa5f33d6727ab1a35ab2ba25f75b
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2024-10-28 15:01:00 -0400

    Update the main.py file to prompt user for options

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from src.day_1.band_name_generator import  generate_band_name
 
 
-def execute():
-    generate_band_name()
+def execute(choice):
+    match choice:
+        case "day_1":
+            generate_band_name()
+        case _:
+            print("Wrong option selected! Please select the correct option.")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    execute()
+    choice = input("Which program do you want to execute? ")
+    execute(choice)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
