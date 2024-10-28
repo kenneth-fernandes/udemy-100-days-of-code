31902c3ebb18921e0bc1a5996160e919542bd633
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2024-10-28 15:03:58 -0400

    Updated the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.day_1.band_name_generator import  generate_band_name
 
 
-def execute(choice):
+def execute_program(choice):
     match choice:
         case "day_1":
             generate_band_name()
@@ -9,9 +9,12 @@ def execute(choice):
             print("Wrong option selected! Please select the correct option.")
 
 
+def execute_main():
+    execute_program(input("Which program do you want to execute? "))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    choice = input("Which program do you want to execute? ")
-    execute(choice)
+    execute_main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
