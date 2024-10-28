83ccd313498dc45b8196336dd32ed1c3dceff2cf
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2024-10-28 14:52:22 -0400

    Adding day 1 python implementation - Band name generator

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.day_1.band_name_generator import  generate_band_name
+
+
+def execute():
+    generate_band_name()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    execute()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
